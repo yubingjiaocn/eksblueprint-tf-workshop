b83ba30eaaac3bd9ed656a4e8fc332c8cd78cfe6
yubingjiaocn <yubingjiaocn@gmail.com>
2022-10-25 12:48:22 +0800

    Minor bug fix

--- init.sh
+++ init.sh
@@ -8,17 +8,28 @@ curl -O "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 unzip -q -o awscli-exe-linux-x86_64.zip
 sudo ./aws/install
 rm awscli-exe-linux-x86_64.zip
+whereis aws
 
 # Install kubectl
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
 chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 echo "source <(kubectl completion bash)" >> ~/.bashrc
+whereis kubectl
 
 # Install eksctl and move to path
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
+whereis eksctl
 
 # Install helm
 curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm repo add eks https://aws.github.io/eks-charts
 helm repo update
+
+AWS_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+echo "export AWS_DEFAULT_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+aws configure set default.region ${AWS_REGION}
+
+ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
